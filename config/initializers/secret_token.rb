1829fafd3e19c4f50676563ee93325e15fb7bfdf
musa deola <info@dengiwebwox.com>
2011-10-23 20:35:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4ed9a3c0d800070f7f473f3aaa7f0073db0380a2b7edd6ec13bce3e3eb7519bfceeb224050500acac9406e2661cb0ce9826214e86446862fb811a37183533686'
